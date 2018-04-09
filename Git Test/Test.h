f6e50763798f8f822275b56bb9fadb354be6019e
Hasaan Ahmed <hasaanahmed@Hasaans-Mac.local>
2018-04-09 02:52:06 -0700

    added a test class

---
+++ Git Test/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  Git Test
+//
+//  Created by Hasaan Ahmed on 09/04/2018.
+//  Copyright © 2018 Hasaan Ahmed. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ Git Test/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  Git Test
+//
+//  Created by Hasaan Ahmed on 09/04/2018.
+//  Copyright © 2018 Hasaan Ahmed. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
